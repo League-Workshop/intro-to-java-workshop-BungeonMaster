b6f569b61335ec13a535cc84d00646da84f666b8
root <root@d05a633a7758>
2020-06-23 18:57:08 +0000

    Worked on Section 2
    Finished MyFirstJavaProgram, Foursquare

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,22 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
+		Robot rob = new Robot();
+		int speed = 100;
+		speed += 50;
+		speed -= 50;
+		speed *= 10;
+		speed /= 5;
 		
+		rob.penDown();
+		rob.setSpeed(speed);
+		
+		for (int i = 0; i < 4; i++) {
+			
+			rob.move(100);
+			rob.turn(90);
+			
+		}
 		
 	}
 }
